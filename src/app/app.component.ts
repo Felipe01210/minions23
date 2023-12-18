6546ba5a885fac882f0e0ff1bff7e6aa9cb8223b
Felipe <erlipe16@gmail.com>
2023-12-18 12:20:24 +0100

    filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { MinionsComponent } from './minions/minions.component';
+import { Minion } from '../app/interfaces/minions.interface';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,83 @@ import { MinionsComponent } from './minions/minions.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+//data.txt
+minions: Minion[] = [
+  {
+    name: "Kevin",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/kevin.jpg",
+    birth: "1951",
+    side:"de los buenos"
+  },
+  {
+    name: "Josua",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/Josua.jpg",
+    birth: "1672",
+    side:"malvado"
+  },
+  {
+    name: "Dave",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/dave.jpg",
+    birth: "1723",
+    side: "de los buenos"
+  },
+  {
+    name: "Mudito",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/mudito.jpeg",
+    birth: "1379",
+    side:"de los buenos"
+  },
+  {
+    name: "Llongueras",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/llongueras.jpg",
+    birth: "1687",
+    side: "malvado"
+  },
+  {
+    name: "Minioncé",
+    bio: "Le va el cante, dar la nota, ama los karaokes, es el rey y reina de la fiesta. Invítalo a tu fiesta o te arrepentirás.",
+    img: "assets/img/minionce.jpg",
+    birth: "1976",
+    side: "de los buenos"
+  },
+  {
+    name: "Lobeznion",
+    bio: "No lo enfades, este bichillo tiene muy malas pulgas...aunque sólo mide medio metro y ¡no puede ser más gracioso!",
+    img: "assets/img/lobeznion.jpg",
+    birth: "2017",
+    side: "malvado"
+  },
+  {
+    name: "Minion Presley",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/minion-presley.jpg",
+    birth: "2017",
+    side: "malvado"
+  }
+];
+
   title = 'pruebasAngular';
+
+
+//filtrados
+filtrados: Minion[] = this.minions;
+
+
+  filterQuery(query: string){
+    if(query.length>0){
+      this.filtrados = this.minions.filter(minion => minion.name.includes(query));
+    }else{
+      this.filtrados = this.minions;
+    }
+  }
+
+  searchMinion(query: string){
+    this.filterQuery(query)
+  }
 }
--- src/app/minions/minions.component.ts
+++ src/app/minions/minions.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Minion } from '../interfaces/minions.interface';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-minions',
@@ -10,66 +11,20 @@ import { Minion } from '../interfaces/minions.interface';
 })
 export class MinionsComponent {
 
-  //data.txt
-  minions: Minion[] = [
-    {
-      name: "Kevin",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/kevin.jpg",
-      birth: "1951",
-      side:"de los buenos"
-    },
-    {
-      name: "Josua",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/Josua.jpg",
-      birth: "1672",
-      side:"malvado"
-    },
-    {
-      name: "Dave",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/dave.jpg",
-      birth: "1723",
-      side: "de los buenos"
-    },
-    {
-      name: "Mudito",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/mudito.jpeg",
-      birth: "1379",
-      side:"de los buenos"
-    },
-    {
-      name: "Llongueras",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/llongueras.jpg",
-      birth: "1687",
-      side: "malvado"
-    },
-    {
-      name: "Minioncé",
-      bio: "Le va el cante, dar la nota, ama los karaokes, es el rey y reina de la fiesta. Invítalo a tu fiesta o te arrepentirás.",
-      img: "assets/img/minionce.jpg",
-      birth: "1976",
-      side: "de los buenos"
-    },
-    {
-      name: "Lobeznion",
-      bio: "No lo enfades, este bichillo tiene muy malas pulgas...aunque sólo mide medio metro y ¡no puede ser más gracioso!",
-      img: "assets/img/lobeznion.jpg",
-      birth: "2017",
-      side: "malvado"
-    },
-    {
-      name: "Minion Presley",
-      bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
-      img: "assets/img/minion-presley.jpg",
-      birth: "2017",
-      side: "malvado"
-    }
-];
+  @Input() minions: Minion[] = [];
+
+//favoritos
+favoritos: Minion[] = [];
+
+//funciones
+addFav(minion: Minion){
+  this.favoritos.push(minion);
+}
 
 
+
+quitFav(minion: Minion){
+  this.favoritos.splice(this.favoritos.indexOf(minion));
+}
 
 }
